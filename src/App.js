e7f33138c8b09de3fdc45e3ec8f99e0b360969fe
ryanramb <ryanrambaran.fl@gmail.com>
2021-10-20 11:38:21 -0400

    finished product

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
 
-function App() {
+import salo1 from './assets/Salo3.jpeg';
+import salo2 from './assets/Salo4.jpeg';
+import salo3 from './assets/Salo5.jpeg';
+import salo4 from './assets/Salo2.jpeg';
+import salo5 from './assets/Salo1.jpeg';
+
+const images = [salo1, salo2, salo3, salo4, salo5];
+
+const App = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const handleChange = () => {
+    const imagesLength = images.length - 1;
+    setCurrentImage((currentImage) => {
+      return currentImage < imagesLength ? currentImage + 1 : 0;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section>
+      <header>
+        <h1>Salomon ADV</h1>
+        <h2>
+          {/* br is inserted for formatting */}
+          Editorial: Salomon Advanced
+          <br />
+          Photography by Asato Iida
+        </h2>
       </header>
-    </div>
+      <figure>
+        <figcaption>
+          {currentImage + 1} / {images.length}
+        </figcaption>
+        <img alt='' src={images[currentImage]} onClick={handleChange} />
+      </figure>
+    </section>
   );
-}
+};
 
 export default App;
